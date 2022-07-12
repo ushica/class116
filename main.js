e739178940c65ce22d3184493f6e1ff335c8b913
ushica <85010308+ushica@users.noreply.github.com>
2022-07-12 14:41:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage ('https://i.postimg.cc/7LwwHQWd/580b57fbd9996e24bc43bed5.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', getPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.lenght > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw() {
+    Image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(noseX, noseY, 20);
+    Image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
